2ed31353ae9cf609126ebde17fb490e03dc08ec5
gabrielepelliccioni13 <115414129+gabrielepelliccioni13@users.noreply.github.com>
2022-12-06 20:11:18 +0100

    Add files via upload
    Final CommandLine

--- CommandLine.sh
+++ CommandLine.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-#In this Command Line we used "pydf" through the simple command "pip install pydf" 
+#In this Command Line we used "datamash" for 1), "pydf" for 2) and "AWK" for 3):
 
 
 # 1) Which location has the maximum number of purchases been made?
-echo "The location with the max number of purchases is:" 
-cat bank_transactions.csv | p.df 'df[["CustLocation","TransactionID"]].groupby("CustLocation").count()' 'df.sort_values(by="TransactionID",ascending=False).head(1)' 'df.reset_index()' -o table
+echo "1) The location with the max number of purchases is:"
+awk 'BEGIN{FS= ","; OFS = "\t"}
+NR==1 {print "CustomerID","TransactionAmount (INR)"}' bank_transactions.csv
+datamash -t, -s groupby 5 count 1 < bank_transactions.csv  | sort -t, -k 2 -nr | head -1
+
+printf "\n"
 
 # 2) In the dataset provided, did females spend more than males, or vice versa?
-echo "In the provided dataset, the males spend more,in fact:"
+echo "2) In the provided dataset, the males spend more,in fact:"
 cat bank_transactions.csv | p.df 'df[["TransactionAmount (INR)","CustGender"]].groupby("CustGender").count()' 'df.sort_values(by="TransactionAmount (INR)",ascending=False).head()' 'df.reset_index()' -o table
 
+printf "\n"
+
 # 3) Report the customer with the highest average transaction amount in the dataset:
-echo "The customer with the highest average T.A.(INR) in the dataset is:"
-cat bank_transactions.csv | p.df 'df[["CustomerID","TransactionAmount (INR)"]].groupby("CustomerID").agg(np.mean)' 'df.sort_values(by="TransactionAmount (INR)",ascending=False).head(1)' 'df.reset_index()' -o table 
+echo "3) The customer with the highest average T.A.(EUR) in the dataset is:
+CustomerId AVG_TA"
+awk -F, '{ListCustomer[$2]+=$9; count[$2]+=1} END{for (Id in ListCustomer){print Id , (ListCustomer[Id]/count[Id])/86.3077,"EUR"}}' bank_transactions.csv | sort -n -k 2  | tail -1
+
 
